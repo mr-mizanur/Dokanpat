4a84ee505bed942f55cfaecd290844bd3c7cccc2
mizanurcodes-ux <mizanur.codes@gmail.com>
2026-08-17 14:02:15 +0600

    reflex logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,13 @@
-export default function Logo() {
+import React from 'react';
+
+
+
+const Logo= () => {
   return (
-    <div className="flex items-center gap-2.5 group cursor-pointer">
-      <div className="flex flex-col">
-        <span className="text-xl font-black tracking-wider text-slate-900 leading-tight">
-          Dokan<span className="text-orange-600">pat</span>
-        </span>
-        <span className="text-xs font-bold tracking-wide text-orange-600">
-          দোকানপাট
-        </span>
-      </div>
+   <div>
+      <img src="/Dokanpat.PNG" alt="Logo"  className='w-35 h-35'/>
     </div>
   );
-}
+};
+
+export default Logo;   
