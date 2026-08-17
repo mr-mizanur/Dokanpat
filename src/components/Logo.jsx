822c69df4d301d735c1dea469bbd54e94ba7cd6e
mizanurcodes-ux <mizanur.codes@gmail.com>
2026-08-17 14:12:03 +0600

    resobl image size

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const Logo= () => {
   return (
    <div>
-      <img src="/Dokanpat.png" alt="Logo"  className='w-35 h-35'/>
+      <img src="/Dokanpat.png" alt="Logo"  className='w-30 h-30'/>
     </div>
   );
 };
